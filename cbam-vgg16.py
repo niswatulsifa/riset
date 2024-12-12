c15255a600aa42cf52e9e660124f83b12d543ec6
Niswatul Sifa <111745245+niswatulsifa@users.noreply.github.com>
2024-12-12 23:00:02 +0700

    Update cbam-vgg16.py

--- cbam-vgg16.py
+++ cbam-vgg16.py
@@ -36,9 +36,8 @@ def load_model(model_path):
             raise FileNotFoundError(f"File model tidak ditemukan di {model_path}")
         
         # Memuat model dengan TensorFlow
-        model_path = os.path.join(os.getcwd(), 'best_model.h5')  # Pastikan file berada di direktori yang benar
-        model = tf.keras.models.load_model(model_path)
-        #model = tf.keras.models.load_model(model_path)
+        #model_path = os.path.join(os.getcwd(), 'best_model.h5')  # Pastikan file berada di direktori yang benar
+        model = tf.keras.models.load_model(model)
         return model
     except Exception as e:
         st.error(f"Error saat memuat model: {e}")
